a5c4f8c975754af5c2d8b287123f20d5b570a9e6
TanishaChawla <chawlatanisha20@gmail.com>
2016-07-04 21:20:04 +0530

    chair added

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -2,6 +2,10 @@
  * Created by systems on 26-06-2016.
  */
 var teamleads=[
+    ["Chair","Kanishk Sanghi"],
+    ["Co-Chair","Mythili H","Shivanand K"],
+    ["Sponsorships Team","Amogh Sharma","Avinash Parkala"],
+    ["Research Team","Anna Shajee","Fazal Ahmad"],
     ["Logistics and Transportation Team","Anshit Saxena","Siddharth S Narayanan"],
     ["Photography and Video Team","Aseem Jain"],
     ["Design Team","Hemanth Sai C","Ruchi Agarwal"],
@@ -9,14 +13,15 @@ var teamleads=[
     ["Purchase and Finance Team","Omkar Ubale","Khyatee Boroowa"],
     ["Documentation Team","Krithika Ravi","Nirmal Padgelwar"],
     ["Hospitality and Guestcare Team","Saptarshi bhuyan","Shubham Mittal"],
-    ["Research Team","Anna Shajee","Fazal Ahmad"],
     ["Miscellaneous Team","Nandini Sharma","Sai Tarang","Ayushi Chaube","Varun Chopra"],
-    ["Sponsorships Team","Amogh Sharma","Avinash Parkala"],
     ["Registration Team","Arjita Singh","Prathik Sunku"],
     ["Onstage Team","Arundhati Gaddala","Sriram Srinivasan"],
     ["Quality Assurance and Assistance Team","Ananya Kar"]
 ];
 var mentors=[
+    ["Chair","Harsh Shah"],
+    ["Sponsorships Team","Andrew Ashish","Parikshit Mishra"],
+    ["Research Team","Arpita Kulkarni","Sreecharan Challa"],
     ["Logistics and Transportation Team","Hemant Khatwani","Anush Baskaran"],
     ["Photography and Video Team","Anand Jain"],
     ["Design Team","Vishwanath Singh","Sreekar Reddy"],
@@ -24,9 +29,7 @@ var mentors=[
     ["Purchase and Finance Team","Vamsi Krishna Thadichetty","Richard T V"],
     ["Documentation Team","Shreyansh Shukla","Anupama P. Joy"],
     ["Hospitality and Guestcare Team","Himanshu Grover","Shubham Gangwal"],
-    ["Research Team","Arpita Kulkarni","Sreecharan Challa"],
     ["Miscellaneous Team","Ekta Pandey","Rahul Lella","Nayaab Singh","Anirudh Arunan"],
-    ["Sponsorships Team","Andrew Ashish","Parikshit Mishra"],
     ["Registration Team","Shubham Sagar","Sahiba Kaur"],
     ["Onstage Team","Ritwik Raje","Oindrila Banerjee"],
     ["Quality Assurance and Assistance Team","Harshitha Reddy Pulichintha"]
